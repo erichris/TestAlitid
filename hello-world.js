80977fd13c7d3648a384223c4bec38365e8900e7
erichris <erichris@live.com.mx>
2018-12-18 17:35:24 -0600

    asd

--- hello-world.js
+++ hello-world.js
@@ -1,7 +1,87 @@
-var http = require("http");
+var io = require('socket.io')(process.env.PORT || 26495);
+const { Pool, Client } = require('pg')
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(26495);
+console.log('Database starting...');
+
+const pool = new Pool({
+  user: 'alitid_1',
+  host: 'localhost',
+  database: 'alitid',
+  password: 'Nomorelove12',
+  port: 5432,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  //console.log(err, res)
+  pool.end()
+})
+
+const client = new Client({
+  user: 'alitid_1',
+  host: 'localhost',
+  database: 'alitid',
+  password: 'Nomorelove12',
+  port: 5432,
+})
+
+/*
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Playeras',
+  password: 'Nomorelove12',
+  port: 5432,
+})
+*/
+
+client.connect()
+
+console.log('Database started');
+
+
+
+
+console.log('Server starting');
+io.on('connection', function(socket){
+  console.log('client connected');
+
+  //socket.emit('login', 'Par1', 'Par2');
+
+  socket.on('Login', function(user, pass, deviceID){
+    console.log("OnMessage");
+    socket.emit('test', 'MyNick', 'Msg to the client');
+    
+	var MyQuery = 'SELECT id, app FROM public."Login_planescape" WHERE "ID1" = '; 
+	MyQuery += "'" + user + "'";
+	MyQuery += ' AND "ID2" = ';
+	MyQuery += "'" + pass + "'"
+	MyQuery += ';';
+	console.log(MyQuery);
+	
+    client.query(MyQuery, (err, res) => {
+	  //console.log(err, res.rows[0])
+	  if(res.rows[0] != null){
+		  socket.emit('Loged', res.rows[0]['id'], res.rows[0]['app']);
+	  }else{
+		  socket.emit('NotLoged', "Usuario o Contraseña incorrectos");
+	  }
+      //client.end()
+      return res;
+    })
+    
+  });
+
+/*
+  socket.on('loggin', function(param1, param2){
+    console.log("Try Login Pass");
+    
+    client.query('SELECT entrada FROM comidas WHERE db_id = 1;', (err, res) => {
+      console.log(err, res.rows[0]['entrada'])
+      client.end()
+      return res;
+    })
+  });
+*/
+
+});
+
